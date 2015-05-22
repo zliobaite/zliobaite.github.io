2651c82e8e3a9889a63bca26b3c5be09765dee30
inzl <zliobaite@gmail.com>
2015-05-22 23:21:44 +0300

    more pic

--- assets/code/run_Berksons_paradox.R
+++ assets/code/run_Berksons_paradox.R
@@ -8,8 +8,8 @@ jobs <- runif(500, 0, 100)
 cojo <- contests + jobs
 ind <- which(cojo>=150)
 
-png('fig_Berksons.png',width = 6, height = 7.5, units = 'cm', res = 160)
-plot(contests,jobs,cex = 0.5,cex.lab=0.5, cex.axis=0.5,xlab = 'score of jobs',ylab = 'score of contests')
+png('fig_Berksons.png',width = 280,height = 300)
+plot(contests,jobs,xlab = 'score of jobs',ylab = 'score of contests')
 abline(a = 150,b = -1,col='red',lwd=2)
 points(contests[ind],jobs[ind],col='red',cex=0.5)
 dev.off()
