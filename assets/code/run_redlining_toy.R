e3d42e6c546593ba4fbbbcec5435abf29d1776c7
inzl <zliobaite@gmail.com>
2015-05-22 22:56:15 +0300

    bandau kaip su figurom

--- assets/code/run_redlining_toy.R
+++ assets/code/run_redlining_toy.R
@@ -11,16 +11,16 @@ th <- (age-20)/20/2 + 0.5
 income <- income*th
 risk <- 0.8*(10000-income)/10000 + 0.2*(40-age)/20 
 
-png('fig_risk_income.png',width = 6, height = 7.5, units = 'cm', res = 160)
-plot(income,risk,cex = 0.5,cex.lab=0.5, cex.axis=0.5,xlab = 'income',ylab = 'risk')
+png('fig_risk_income.png',width = 400, height = 400)
+plot(income,risk,pch = 20,xlab = 'income',ylab = 'risk')
 dev.off()
 
 png('fig_risk_age.png',width = 6, height = 7.5, units = 'cm', res = 160)
-plot(age,risk,cex = 0.5,cex.lab=0.5, cex.axis=0.5,xlab = 'age',ylab = 'risk')
+plot(age,risk,cex = 0.3,cex.lab=0.5, cex.axis=0.5,xlab = 'age',ylab = 'risk')
 dev.off()
 
 png('fig_income_age.png',width = 6, height = 7.5, units = 'cm', res = 160)
-plot(income,age,cex = 0.5,cex.lab=0.5, cex.axis=0.5,xlab = 'income',ylab = 'age')
+plot(income,age,cex = 0.3,cex.lab=0.5, cex.axis=0.5,xlab = 'income',ylab = 'age')
 dev.off()
 
 data <- as.data.frame(cbind(risk,income,age))
